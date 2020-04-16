ebd7c4a003d576a2bc34bba90b66ec6c469e6c63
insinfo <insinfo2008@gmail.com>
2020-04-16 18:11:45 -0300

    change uploadFiles do List<dynamic> files

---
+++ lib/src/util.dart
@@ -0,0 +1,14 @@
+
+/*function readFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+
+    reader.onerror = reject;
+
+    reader.readAsArrayBuffer(file);
+  })
+}*/
